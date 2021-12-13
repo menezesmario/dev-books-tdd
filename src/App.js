9a8da61609314bb0a54ea5829372aedc82f93896
Mário Menezes <mario082@gmail.com>
2021-12-13 09:40:01 -0300

    renderBooks (refactory book list)

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@ import { Typography } from "@material-ui/core";
 import React from 'react';
 
 function App() {
+  const books = [
+    { name: 'Refactoring' },
+    { name: 'Domain-driven design' }
+  ];
+
+
+
   return (
     <>
      <Typography variant="h2" component="h2" data-test="heading">DevBook!</Typography>
       <div data-test="book-list">
-        <div className="book-item">
-          <h2 className="titles">Refactoring</h2>
-        </div>
-        <div className="book-item">
-          <h2 className="titles">Domain-driven design</h2>
-        </div>
+        {
+          books.map(book => (
+            <div className="book-item">
+              <h2 className="titles">{book.name}</h2>
+            </div>
+          ))
+        }
       </div>
     </>
   );
